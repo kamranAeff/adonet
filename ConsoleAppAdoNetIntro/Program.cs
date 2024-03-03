59fe560ee0247319718d569c96d726fcfd98bd57
kamranAeff <kamran_aeff@mail.ru>
2024-03-03 12:56:20 +0400

    - SqlConnection / CommectionStrings - SqlDataAdapter - SqlCommand - SqlDataReader - DataTable - DataRow

---
+++ ConsoleAppAdoNetIntro/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleAppAdoNetIntro
+{
+    internal class Program
+    {
+        const string cString = "Data Source=.;Database=ShopDb;Trusted_Connection=True;Encrypt=false";
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(cString))
+            using (var command = new SqlCommand("select [Id],[Name],[SmallName] from [dbo].[Sizes]", connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (reader.Read())
+                    {
+                        Console.Write($"{reader["Id"]}  ");
+                        Console.Write($"{reader["Name"]}  ");
+                        Console.WriteLine($"{reader["SmallName"]}");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void WorkingWithSqlDataReader()
+        {
+            using (var connection = new SqlConnection(cString))
+            using (var command = new SqlCommand("select [Id],[Name],[SmallName] from [dbo].[Sizes]", connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (reader.Read())
+                    {
+                        Console.Write($"{reader["Id"]}  ");
+                        Console.Write($"{reader["Name"]}  ");
+                        Console.WriteLine($"{reader["SmallName"]}");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void WorkingSqlDataAdapter()
+        {
+            var connection = new SqlConnection(cString);
+
+            var adapter = new SqlDataAdapter("select [Id],[Name],[SmallName] from [dbo].[Sizes]", connection);
+
+            var table = new DataTable();
+
+            adapter.Fill(table);
+
+            foreach (DataRow row in table.Rows)
+            {
+                Console.Write($"{row["Id"]}  ");
+                Console.Write($"{row["Name"]}  ");
+                Console.WriteLine($"{row["SmallName"]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # adonet
+
+# select [Id],[Name],[SmallName] from [dbo].[Sizes]
+
+IN SQL                   C# (adonet)
+
+DataBase                 DataSet
+Tables                   DataTable
+Row					     DataRow
+Column				     Column
+
+[Ado Net Introduction](https://www.javatpoint.com/ado-net-introduction)
+[ConnectionString](https://www.connectionstrings.com)
+
+### Windows Authentification
+> Data Source=.;Database=ShopDb;Trusted_Connection=True;Encrypt=false;
+
+### SqlServer Authentification
+> Data Source=.;Database=ShopDb;User Id=sa;Password=query;Encrypt=false;
